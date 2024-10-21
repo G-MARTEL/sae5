3b61299338cdc0adaeeb20e9ada0f043490c95da
quentin-massoulle <quentin.massoulledu80@gmail.com>
2024-10-21 11:46:12 +0200

    Creation Route Pour CreationBDD

--- web/routes/web.php
+++ web/routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\BDDController;
 
 Route::get('/', function () {
     return view('accueil');
 })->name('home');
+
+
+Route::get('BDD',[BDDController::class, 'CreateBDD']);
 
 Route::get('Qui-Somme-Nous', function () {
     return view('prensentation');
