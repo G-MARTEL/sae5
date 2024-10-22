5421dc51a51d6ce67a863de478be6cdd92b997b8
quentin-massoulle <quentin.massoulledu80@gmail.com>
2024-10-22 16:27:09 +0200

    re modif Connexion

--- web/app/Http/Controllers/Autentification.php
+++ web/app/Http/Controllers/Autentification.php
@@ -16,28 +16,7 @@ class Autentification extends Controller
     public function login(Request $request)
     {
 
-        try {
-        // Récupérer le nom de la base de données
-        $databaseName = $pdo->getAttribute(PDO::ATTR_DATABASE);
-
-        // Récupérer les tables de la base de données
-        $query = 'SELECT name FROM sqlite_master WHERE type="table"';
-        $stmt = $pdo->query($query);
-        $tables = $stmt->fetchAll(PDO::FETCH_COLUMN);
-
-        // Vérifier si la table spécifiée existe
-        $tableExists = in_array($tableToCheck, $tables);
-
-        return [
-            'database' => $databaseName,
-            'tables' => $tables,
-            'tableExists' => $tableExists,
-        ];
-    } catch (PDOException $e) {
-        return [
-            'error' => 'Erreur lors de la connexion à la base de données: ' . $e->getMessage(),
-        ];
-    }
+        
         $mdp =$request -> password;
         $email = $request -> email;
         $account = DB::table('accounts')->where('email', $request->email)->first();
