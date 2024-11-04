a8a9c3ce28debe4175b5f6374fecaf0db3a80831
DuboissetHugo <duboissethugo@gmail.com>
2024-11-04 10:28:11 +0100

    prestations front (ajouter le php quand la bdd sera OK)

---
+++ web/resources/views/prestation.blade.php
@@ -0,0 +1,55 @@
+@extends('layout')
+
+@section('styles')
+@endsection
+
+
+@section('scripts')
+@endsection
+@section('content')
+
+
+<!-- <div class="container" id="prestations-1">
+    <div class="container-inner">
+        <h2>Nos prestations</h2>
+    </div>
+</div>
+
+<div class="container" id="prestations-2">
+    <div class="container-inner">
+        <div class="colonnes">
+            <div class="colonne texte-colonne">
+                <h3>Assurez-vous une offre de qualité</h3>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et 
+                    quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur 
+                    aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+            </div>
+            <div class="colonne images-colonne">
+                <h3>Notre équipe</h3>
+                <div class="colonnes">
+                    <div class="colonne">
+                        <figure>
+                            <img src="{{ asset('assets/presentation/employe1.jpg') }}" alt="Photo de l'équipe">
+                        </figure>
+                    </div>
+                    <div class="colonne">
+                        <figure>
+                            <img src="{{ asset('assets/presentation/employe2.jpg') }}" alt="Photo de l'équipe">
+                        </figure>
+                    </div>
+                    <div class="colonne">
+                        <figure>
+                            <img src="{{ asset('assets/presentation/employe3.jpg') }}" alt="Photo de l'équipe">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+
+
+
+@endsection
--- web/resources/views/prestations.blade.php
+++ web/resources/views/prestations.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 
 @section('styles')
+<link rel="stylesheet" href="./css/prestations.css"/>
 @endsection
 
 
@@ -8,8 +9,36 @@
 @endsection
 @section('content')
 
+<?php $texte = "Ceci est un exemple de texte très long qui pourrait contenir plus de 150 caractères, et que l'on veut afficher en version tronquée pour un aperçu rapide.";
+$texteLimite = substr($texte, 0, 150);
 
-<H1>Page des Prestation </H1>
+if (strlen($texte) > 150) {
+    $texteLimite .= '...';
+}
+?>
+
+<div class="container" id="prestations1">
+    <h1>Nos prestations</h1>
+</div>
+
+<!-- ajouter un foreach -->
+<div class="container" id="prestations2">
+    <div class="container-inner">
+        <div class="colonnes">
+            <div class="colonne">
+                <h3> titre</h3>
+                <p><?php echo $texteLimite;
+                    ?></p>
+            </div>
+            <div class="colonne">
+                <img src="{{ asset('assets/presentation/employe2.jpg') }}" alt="">
+            </div>
+            <div class="colonne">
+                <a href="">Accéder à la prestation</a>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 @endsection
