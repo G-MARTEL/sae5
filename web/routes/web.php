c9ce0141443ba6d1fad82e6345a78dafbabfe51b
DuboissetHugo <duboissethugo@gmail.com>
2024-11-06 15:43:12 +0100

    fin page utilisateur, front et back

--- web/routes/web.php
+++ web/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\BDDController;
 use App\Http\Controllers\Autentification;
+use App\Http\Controllers\ClientController;
+
 
 
 Route::get('/', function () {
@@ -29,6 +31,9 @@ Route::get('simulateur', function () {
   return view('simulateur');
 })->name('simulateur') ;
 
+Route::get('test', function () {
+    return view('test');
+  })->name('test') ;
 
 
 Route::get('devis', function () {return view('devis');})->name('devis');
@@ -41,16 +46,20 @@ Route::post('/connexion', [Autentification::class, 'login']);
 
 Route::prefix('client')->name('client.')->group(function() {
 
-    Route::get('/accueil', function () {
-        if (session('role') !== 'client') {
-            return redirect('/'); // Redirige si le rôle n'est pas 'client'
-        }
-        return view('acceuilCliens');
-        
-    })->name('accueil');
+    // Route::get('/accueil', function () {
+    //     if (session('role') !== 'client') {
+    //         return redirect('/'); // Redirige si le rôle n'est pas 'client'
+    //     }
+    //     return view('acceuilCliens');
+
+    // })->name('accueil');
+    Route::get('/accueil', [ClientController::class, 'showClientDashboard'])->name('accueil');
 
 
 });
+
+
+
 Route::prefix('employees')->name('employees.')->group(function() {
     Route::get('/accueil', function () {
         if (session('role') !== 'employee') {
