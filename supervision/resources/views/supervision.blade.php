700122d0c44f38afcdf4a584dc202d5ce9683184
Maxime Bolandard <maxime.bolandard@gmail.com>
2024-11-06 17:10:02 +0100

    Peaufinage du front-end du site de supervision + ajout de la connexion à la BDD

--- supervision/resources/views/supervision.blade.php
+++ supervision/resources/views/supervision.blade.php
@@ -28,42 +28,42 @@
         </thead>
         <tbody>
             <tr>
-                <td>PC1</td>
+                <td class="titleTableau">PC1</td>
                 <td>oui</td>
                 <td>10,9/500Go</td>
                 <td>60%</td>
                 <td>80%</td>
             </tr>
             <tr>
-                <td>PC2</td>
+                <td class="titleTableau">PC2</td>
                 <td class="status-error">non</td>
                 <td>11,8/500Go</td>
                 <td>70%</td>
                 <td>70%</td>
             </tr>
             <tr>
-                <td>PC3</td>
+                <td class="titleTableau">PC3</td>
                 <td>oui</td>
                 <td class="status-warning">415,8/500Go</td>
                 <td class="status-warning">90%</td>
                 <td>60%</td>
             </tr>
             <tr>
-                <td>PC4</td>
+                <td class="titleTableau">PC4</td>
                 <td>oui</td>
                 <td class="status-error">480,9/500Go</td>
                 <td>30%</td>
                 <td>45%</td>
             </tr>
             <tr>
-                <td>DNS</td>
+                <td class="titleTableau">DNS</td>
                 <td>oui</td>
                 <td>6,8/500Go</td>
                 <td>10%</td>
                 <td>58%</td>
             </tr>
             <tr>
-                <td>BDD</td>
+                <td class="titleTableau">BDD</td>
                 <td>oui</td>
                 <td>264,4/500Go</td>
                 <td class="status-error">99%</td>
