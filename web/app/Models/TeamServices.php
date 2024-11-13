a0d8088963c2e86c1a69c171a8348277bc0542fd
quentin-massoulle <quentin.massoulledu80@gmail.com>
2024-11-13 09:51:34 +0100

    creation de tout les model avec la comande

---
+++ web/app/Models/TeamServices.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TeamServices extends Model
+{
+    use HasFactory;
+}
