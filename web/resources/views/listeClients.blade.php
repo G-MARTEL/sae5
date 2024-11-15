230f69dc031ad0948efa859cac43e38eb90ef0d7
quentin-massoulle <quentin.massoulledu80@gmail.com>
2024-11-15 19:13:18 +0100

    debut css liste admin

--- web/resources/views/listeClients.blade.php
+++ web/resources/views/listeClients.blade.php
@@ -1,22 +1,57 @@
-<h1>liste clients</h1>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset('css/listeClient.css') }}"/>
+        <title>Liste-client</title>
+     </head>
+<body>
+    
+<div class="containeur">
+    <div class="container-inner">
+        <h1>liste clients</h1>
+    </div>
+    
+</div>
 
-@foreach ($clients as $clientData)
-    <p>  <strong>Nom et Prénom du client  :</strong> {{$clientData['donneeClient']->first_name.' '.$clientData['donneeClient']->last_name}}
-       <br> <strong>Id : </strong>{{$clientData['clientAccounts']->client_id}}
-    <form action="modifClientAsso" method="post">
-        <input type="hidden" name="client_id" value="{{ $clientData['clientAccounts']->client_id }}">
-        <Strong> Employer Associer :</Strong>
-        @csrf
-        <select name="employee_id" id="employee">
-            @foreach ($listeEmployees as $Employer)
-                <option value="{{ $Employer->employee_id }}" 
-                    {{ $clientData['clientAccounts']->FK_employee_id == $Employer->employee_id ? 'selected' : '' }}>
-                    {{ $Employer->account->first_name . ' ' . $Employer->account->last_name }}
-                </option>
-            @endforeach
-        </select>
-        <button type="submit">Envoyer</button>
-    </form>
-    <br>
-    </p>
-@endforeach
+<div class="containeur">
+    <div class="container-inner">
+        @foreach ($clients as $clientData)
+        <p>  <strong>Nom et Prénom du client  :</strong> {{$clientData['donneeClient']->first_name.' '.$clientData['donneeClient']->last_name}}
+        <br> <strong>Id : </strong>{{$clientData['clientAccounts']->client_id}}
+
+        <form action="modifClientAsso" method="post">
+            @csrf
+            <input type="hidden" name="client_id" value="{{ $clientData['clientAccounts']->client_id }}">
+
+            <Strong> Employer Associer :</Strong>
+
+            <select name="employee_id" id="employee">
+                @php
+                if ($clientData['clientAccounts']->FK_employee_id == null)
+                {
+                    echo '<option value="">Aucun employé associé</option>';
+                }
+                @endphp
+                
+                @foreach ($listeEmployees as $Employer)
+                    <option value="{{ $Employer->employee_id }}" 
+                        {{ $clientData['clientAccounts']->FK_employee_id == $Employer->employee_id ? 'selected' : '' }}>
+                        {{ $Employer->account->first_name . ' ' . $Employer->account->last_name }}
+                    </option>
+                @endforeach
+
+            </select>
+            <button type="submit">Envoyer</button>
+
+        </form>
+        <br>
+        </p>
+        @endforeach
+    </div>
+</div>
+
+
+
+</body>
