4e14e33270652bf488720668be70d10853f20da0
quentin-massoulle <quentin.massoulledu80@gmail.com>
2024-11-22 12:25:52 +0100

    mise en place de la liste des prestation

--- web/resources/views/acceuilAdmin.blade.php
+++ web/resources/views/acceuilAdmin.blade.php
@@ -1,8 +1,36 @@
-<h1>Page acceuil admin</h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrateur</title>
+    <!-- Lien vers le fichier CSS -->
+    <link rel="stylesheet" href="{{ asset('css/listeEmployee.css') }}">
+</head>
 
 
-<a href="listeClients">Acceder a la liste des clients</a>
-<a href="listeEmployee">Acceder a la liste des Employés</a>
+
+<div class="containneur">
+    <div class="container-inner">
+        <h2>Bienvenue, Administrateur!</h2>
+    </div>
+</div>
+
+<div class="containneur">
+    <div class="container-inner">
+        <h2>Gestion des clients</h2>
+        <a href="listeClients">Acceder a la liste des clients</a>
+    </div>
+    <div class="container-inner">
+        <h2>Gestion des Employés</h2>
+        <a href="listeEmployee">Acceder a la liste des employés</a>
+    </div>
+    <div class="container-inner">
+        <h2>Gestion des prestations</h2>
+        <a href="listePrestations">Acceder a la liste des prestations</a>
+    </div>
+</div>
+
+
 
 
 
