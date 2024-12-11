0904b1138bd0850897cc14e4c8e6b738cda75ba9
DuboissetHugo <duboissethugo@gmail.com>
2024-12-11 17:12:59 +0100

    Liste de clients

--- web/resources/views/listeClientAttitres.blade.php
+++ web/resources/views/listeClientAttitres.blade.php
@@ -1,20 +1,33 @@
-<table>
-    <thead>
-        <tr>
-            <th>Nom</th>
-            <th>Prénom</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($clients as $client)
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vos clients</title>
+    <link rel="stylesheet" href="{{ asset('css\employee\listeClientAttitres.css') }}">
+    
+</head>
+<div class="employee-clients">
+    <h1>Mes clients</h1>
+    <table class="clients-table">
+        <thead>
             <tr>
-                <td>
-                    <a href="{{ route('employees.clients.show', $client->client_id) }}">
-                        {{ $client->account->last_name }}
-                    </a>
-                </td>
-                <td>{{ $client->account->first_name }}</td>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>      </th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($clients as $client)
+                <tr>
+                    <td>{{ $client->account->last_name }}</td>
+                    <td>{{ $client->account->first_name }}</td>
+                    <td><a href="{{ route('employees.clients.show', $client->client_id) }}" class="client-link">
+                        Consulter le profil
+                    </a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
