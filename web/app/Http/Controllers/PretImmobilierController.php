bce09cf40753aeed1635e571acc8655a6c2b3e10
DuboissetHugo <duboissethugo@gmail.com>
2025-01-04 19:42:42 +0100

    creation de simulateurs, mise à jour du front, script JS

--- web/app/Http/Controllers/PretImmobilierController.php
+++ web/app/Http/Controllers/PretImmobilierController.php
@@ -12,37 +12,37 @@ class PretImmobilierController extends Controller
         return view('simulateur.form');
     }
 
-    // Traiter la simulation
-    public function simulate(Request $request)
-    {
-        $capital = $request->input('capital');
-        $taux = $request->input('taux') / 100 / 12;
-        $duree = $request->input('duree') * 12; 
-        $apport = $request->input('apport', 0);
-        $assurance = $request->input('assurance', 0) / 100;
+    // // Traiter la simulation
+    // public function simulateImmo(Request $request)
+    // {
+    //     $capital = $request->input('capital');
+    //     $taux = $request->input('taux') / 100 / 12;
+    //     $duree = $request->input('duree') * 12; 
+    //     $apport = $request->input('apport', 0);
+    //     $assurance = $request->input('assurance', 0) / 100;
 
-        // Capital emprunté après apport
-        $capital_emprunte = $capital - $apport;
+    //     // Capital emprunté après apport
+    //     $capital_emprunte = $capital - $apport;
 
-        // Mensualité hors assurance
-        $mensualite = ($capital_emprunte * $taux * pow(1 + $taux, $duree)) /
-                      (pow(1 + $taux, $duree) - 1);
+    //     // Mensualité hors assurance
+    //     $mensualite = ($capital_emprunte * $taux * pow(1 + $taux, $duree)) /
+    //                   (pow(1 + $taux, $duree) - 1);
 
-        // Total des intérêts
-        $total_interets = ($mensualite * $duree) - $capital_emprunte;
+    //     // Total des intérêts
+    //     $total_interets = ($mensualite * $duree) - $capital_emprunte;
 
-        // Assurance mensuelle
-        $assurance_mensuelle = ($capital_emprunte * $assurance) / 12;
-        $mensualite_totale = $mensualite + $assurance_mensuelle;
+    //     // Assurance mensuelle
+    //     $assurance_mensuelle = ($capital_emprunte * $assurance) / 12;
+    //     $mensualite_totale = $mensualite + $assurance_mensuelle;
 
-        // Résultat
-        $resultat = [
-            'capital_emprunte' => $capital_emprunte,
-            'mensualite' => round($mensualite, 2),
-            'total_interets' => round($total_interets, 2),
-            'mensualite_totale' => round($mensualite_totale, 2),
-        ];
+    //     // Résultat
+    //     $resultat = [
+    //         'capital_emprunte' => $capital_emprunte,
+    //         'mensualite' => round($mensualite, 2),
+    //         'total_interets' => round($total_interets, 2),
+    //         'mensualite_totale' => round($mensualite_totale, 2),
+    //     ];
 
-        return view('simulateur.resultat', compact('resultat'));
-    }
+    //     return view('simulateur.resultat', compact('resultat'));
+    // }
 }
